9ea88f3becac1781d6799e0322c015af5f76eb1a
Hoang Nhan <hnhan793@gmail.com>
2023-07-10 15:02:11 +0700

    modify modal quick view product

--- assets/js/collections.js
+++ assets/js/collections.js
@@ -4,3 +4,40 @@ import * as f from '../../app/controllers/Collections/fnc_collections.js';
 f.setUI();
 eventCollection();
 
+// Show size
+
+const selectSize = (size) => {
+    const sizeTitleElement = document.getElementById("sizeTitle");
+    sizeTitleElement.textContent= size;
+  };
+  
+selectSize()
+
+// xử lý chọn số lượng
+let amountElement = document.querySelector('#amount')
+let amount = +amountElement.value
+
+// Xử lý handlePlus
+
+let render = (amount) => {
+    amountElement.value = amount
+}
+
+let handlePlus = () => {
+    amount++
+    render(amount)
+}
+
+// Xử lý handleMInus
+let handleMinus = () => {
+    if(amount > 1){
+        amount--
+        render(amount)
+    }
+}
+
+amountElement.addEventListener('input', () => {
+    amount = +amountElement.value
+    amount = isNaN(amount) ? 1:amount;
+    render(amount)
+})
